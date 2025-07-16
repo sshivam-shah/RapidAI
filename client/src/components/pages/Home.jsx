bb04cb82028cdbcce55c0ddacac1122464772cad
Shivam Shah <shivamshah8989@gmail.com>
2025-07-17 00:35:05 +0530

    first commit

---
+++ client/src/components/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import Hero from '../Hero'
+import AiTools from '../AiTools'
+import Testimonial from '../Testimonial'
+import Plan from '../Plan'
+import Footer from '../Footer'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <AiTools />
+      <Testimonial />
+      <Plan />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
