108dd148e5649f5de81e189cce5dc7d1aa615b07
Shivam Shah <shivamshah8989@gmail.com>
2025-07-17 18:12:48 +0530

    upto client finished and server building

--- client/src/components/pages/RemoveObject.jsx
+++ client/src/components/pages/RemoveObject.jsx
@@ -4,8 +4,9 @@ import React, { useState } from 'react'
 const RemoveObject = () => {
 
   const [input, setInput] = useState("");
+  const [object,setObject] = useState("");
 
-  const onSubmitHandler = async (e) => {
+  const onSubmitHandler = async (e) => { 
     e.preventDefault();
   };
 
@@ -17,7 +18,7 @@ const RemoveObject = () => {
         className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
       >
         <div className="flex items-center gap-3">
-          <Sparkles className="w-6 text-[#FF4938]" />
+          <Sparkles className="w-6 text-[#4A7AFF]" />
           <h1 className="text-xl font-semibold">Object Removal</h1>
         </div>
         <p className="mt-6 text-sm font-medium">Upload image</p>
@@ -28,15 +29,23 @@ const RemoveObject = () => {
           className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
           required
         />
-        <p className="text-xs text-gray-500 font-light mt-1">Supports JPG, PNG, and other image formats</p>
-        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
+<p className="mt-6 text-sm font-medium">Describe object name to remove</p>
+        <textarea
+          onChange={(e) => setObject(e.target.value)}
+          value={object}
+          rows={4}
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          placeholder="e.g., watch or spoon, Only single object name"
+          required
+        />
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
           <Scissors className="w-5" /> Remove object
         </button>
       </form>
       {/* Right col  */}
       <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96">
         <div className="flex items-center gap-3">
-          <Scissors className="w-5 h-5 text-[#FF4938]" />
+          <Scissors className="w-5 h-5 text-[#4A7AFF]" />
           <h1 className="text-xl font-semibold">Processed Image</h1>
         </div>
         <div className="flex-1 flex justify-center items-center">
